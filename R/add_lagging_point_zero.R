d9ce4313d57d4fbbe0f9f9093c9094717c919c4f
Edward Lavender <el72@st-andrews.ac.uk>
2020-05-08 14:13:58 +0100

    Fix add_lagging_point_zero() and update with ignore argument.

--- R/add_lagging_point_zero.R
+++ R/add_lagging_point_zero.R
@@ -3,12 +3,26 @@
 #'
 #' @param x A numeric or character vector which contains some elements to which lagging .0(s) should be added.
 #' @param n A number which defines the desired number of decimal places. Any element in \code{x} with fewer than \code{n} decimal places will have the appropriate number of decimal places added. If \code{n} is less than the number of decimal places for any element in \code{x}, the function will return an error. In this case, round all numbers to the same precision first, before implementing \code{add_lagging_point_zero}.
+#' @param ignore A logical input which specifies whether or not, under the condition that no number has any decimal places, the \code{n} argument should be ignored (i.e. if \code{TRUE}, the character vector, \code{x}, is simply returned without any adjustments).
 #'
 #' @return A character vector, as inputted, but in which any elements with fewer than \code{n} decimal places have had ".0"(s) added.
 #'
 #' @examples
 #'
-#' #### (1) Example 1: prettier axis labels
+#' #### Example (1): Bring all numbers up to the same number of decimal places
+#' add_lagging_point_zero(c(0.01, 0.002), n = 4)
+#'
+#' #### Example (2): More examples
+#' add_lagging_point_zero(seq(0, 1000, by = 100), 1)
+#' add_lagging_point_zero(c(50.123, 1000, 150, 2000), 3)
+#' add_lagging_point_zero(c(50.0, 1000, 150, 2000), 1)
+#'
+#' #### Example (3): the ignore argument returns an unchanged character vector if no numbers
+#' # ... in the vector have decimal places
+#' add_lagging_point_zero(seq(0.1, 1000, by = 100), 1, ignore = TRUE) # decimal places added
+#' add_lagging_point_zero(seq(0, 1000, by = 100), 1, ignore = TRUE)   # decimal places not added
+#'
+#' #### Example (3): Using add_lagging_point_zero() for prettier axis labels
 #' # Numeric vector input
 #' # Define some data/axis positions
 #' at <- seq(0, 1, by = 0.2) * 10^5
@@ -29,9 +43,6 @@
 #' # Labels after before add_lagging_point_zero() are tidier:
 #' axis(side = 2, at, labels = labels2)
 #'
-#' #### (2) Example 2: Bring all numbers up to the same number of decimal places
-#' add_lagging_point_zero(c(0.01, 0.002), n = 4)
-#'
 #' @author Edward Lavender
 #' @export
 #'
@@ -41,13 +52,12 @@
 #### add_lagging_point_zero
 
 add_lagging_point_zero <-
-  function(x, n){
+  function(x, n, ignore = FALSE){
     x <- as.character(x)
-    dp <- stringr::str_split_fixed(x, ".", 3)[, 3]
+    dp <- stringr::str_split_fixed(x, "[.]", 2)[, 2]
+    if(all(nchar(dp) == 0) & ignore) return(x)
     diff <- n - nchar(dp)
-    if(any(diff < 0)){
-      stop("n is too small; some numbers have more decimal places than n.")
-    }
+    if(any(diff < 0)) stop("n is too small; some numbers have more decimal places than n.")
     pwd <- function(x, diff, n){
       if(diff == n){
         paste0(x, ".", paste0(rep(0, diff), collapse = ""))
