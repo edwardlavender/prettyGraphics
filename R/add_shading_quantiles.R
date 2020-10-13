8566fd23a74c742b5839c6f44582b4fee3f1fcdf
Edward Lavender <el72@st-andrews.ac.uk>
2020-10-13 14:07:32 +0100

    Update add_shading_quantiles() to return a list of arguments for add_colour_bar().

--- R/add_shading_quantiles.R
+++ R/add_shading_quantiles.R
@@ -5,17 +5,45 @@
 #' @param cols A character vector of colours. This should be of the same length as \code{probs}.
 #' @param horiz A logical input that defines whether or not shading is added horizontally or vertically across a plot (i.e., whether \code{x} is the x or y variable on a given plot). This is passed to \code{\link[prettyGraphics]{add_shading_bar}}.
 #' @param lim A numeric input that defines the horizontal or vertical limits. This is passed to passed to \code{\link[prettyGraphics]{add_shading_bar}}.
-#' @param return_dat A logical input which defines whether or not to return a dataframe with probabilities, colours and quantiles.
+#' @param return_list A logical input that defines whether or not to return a list of outputs (see Value).
 #' @param ... Additional arguments passed to \code{\link[prettyGraphics]{add_shading_bar}}.
-#' @return This function adds shading to a plot to show to the sample quantiles corresponding to inputted probabilities. A dataframe with probabilities ('prob'), colours ('col') and quantiles ('quantile') is also returned if \code{return_dat} is \code{TRUE}.
+#' @return This function adds shading to a plot to show the sample quantiles corresponding to inputted probabilities. A named list of outputs is also returned if \code{return_list = TRUE}. This contains: (1) 'dat', a dataframe with probabilities ('prob'), colours ('col') and quantiles ('quantile'); (2) 'data_legend', a dataframe that contains only probabilities ('x') and colours ('col'); and (3) 'pretty_axis_args', a list of suggested arguments for pretty axes. 'data_legend' and 'pretty_axis_args' can be passed directly to \code{\link[prettyGraphics]{add_colour_bar}} to add a colour bar to the plot.
+#'
 #' @examples
 #' # Simulate some normally distributed observations
 #' n <- 100
 #' x <- stats::rnorm(100, -200, 200)
+#' pp <- par(oma = c(2, 2, 2, 4))
+#'
+#' # Create a blank plot with appropriate axis limits
 #' axis_ls <- pretty_plot(x, type = "n", return_list = TRUE)
-#' # Add shading for the quantiles of observations
-#' add_shading_quantiles(x, horiz = TRUE, lim = axis_ls[[1]]$lim, border = NA)
+#'
+#' # Add shading for the quantiles of observations, saving the
+#' # ... list returned by the function so we can add a colour bar later.
+#' asq_ls <- add_shading_quantiles(x,
+#'                                 horiz = TRUE,
+#'                                 lim = axis_ls[[1]]$lim,
+#'                                 border = NA,
+#'                                 return_list = TRUE)
+#'
+#' # Add observed points onto the plot
 #' points(1:n, x)
+#'
+#' # Add a colour bar via TeachingDemos::subplot() and
+#' # ... prettyGraphics::add_colour_bar()
+#' # ... using outputs returned by function:
+#' TeachingDemos::subplot(fun =
+#'                          add_colour_bar(
+#'                            data_legend = asq_ls$data_legend,
+#'                            pretty_axis_args = asq_ls$pretty_axis_args
+#'                          ),
+#'                        x = axis_ls[[1]]$lim[2] + 1,
+#'                        y = axis_ls[[2]]$lim[1],
+#'                        size = c(0.2, 2.8),
+#'                        vadj = 0, hadj = 0
+#' )
+#' par(pp)
+#'
 #' @author Edward Lavender
 #' @export
 #'
@@ -26,7 +54,7 @@ add_shading_quantiles <-
            cols = scales::alpha("dimgrey", ifelse(probs > 0.5, 1-probs, probs) + 0.05),
            horiz = TRUE,
            lim,
-           return_dat = TRUE,...){
+           return_list = TRUE,...){
 
     #### Define dataframe
     dat <- data.frame(prob = probs, col = cols)
@@ -39,7 +67,23 @@ add_shading_quantiles <-
                     lim = lim,
                     col = dat$col[1:(nrow(dat)-1)],...) %>% invisible()
 
-    #### Return dat
-    if(return_dat) return(dat)
+    #### Define outputs
+    if(return_list){
+      ## Dataframe for add_colour_bar()
+      data_legend <- data.frame(x = dat$prob, col = dat$col)
+      ## Suggested pretty_axis_args for add_colour_bar()
+      pretty_axis_args <- list("4" = NULL)
+      pretty_axis_args[[1]]$axis <- list(side = 4,
+                                         at = seq(0, 1, by = 0.25),
+                                         pos = 1,
+                                         las = TRUE)
+      pretty_axis_args[[1]]$lim <- c(0, 1)
+      attributes(pretty_axis_args[[1]]$lim)$user <- c(FALSE, FALSE)
+      out <- list(dat = dat,
+                  data_legend = data_legend,
+                  pretty_axis_args = pretty_axis_args)
+      return(out)
+    }
+
   }
 
