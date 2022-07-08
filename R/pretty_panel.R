b5ce9f623e66e848ddfb4f3ff871488323143976
Edward Lavender <el72@st-andrews.ac.uk>
2022-07-08 16:52:18 +0200

    Update pretty_panel() with the flexibility to put panel titles in boxes.

--- R/pretty_panel.R
+++ R/pretty_panel.R
@@ -3,7 +3,8 @@
 #' @param x,y,by Vectors that define point coordinates (\code{x, y}) and a grouping variable (\code{by}).
 #' @param data (optional) A dataframe that contains additional information (see \code{add_additional}).
 #' @param xlim,ylim,pretty_axis_args Axis controls. \code{xlim} and \code{ylim} control axis limits for all plots. \code{pretty_axis_args} is a named list of arguments, passed to \code{\link[prettyGraphics]{pretty_axis}}, for further control.
-#' @param add_xlab,add_ylab,add_main (optional) Named lists of arguments, passed to \code{\link[graphics]{mtext}}, to add axis titles to plots. Panel titles are added to each panel, while only  global labels are added for the x and y axes. Empty lists specify default arguments. In this case, panel titles are given as capitalised letters or numbers (if there are more than 26 predictors), in bold, plus the grouping level in brackets; e.g., \strong{A} (Group One) or \strong{1} (Group One). X and y axis labels are simply given as \code{x} and \code{y}. Alternatively, names can be specified via the `text' argument to \code{\link[graphics]{mtext}}. \code{NULL} suppress these arguments.
+#' @param add_xlab,add_ylab,add_main (optional) Named lists of argument to add panel and axis titles. \code{add_xlab} and \code{add_ylab} are passed to \code{\link[graphics]{mtext}} and \code{add_main} is passed to  \code{\link[graphics]{mtext}} if \code{add_main_box = FALSE} or \code{\link[graphics]{legend}} otherwise. Panel titles are added to each panel, while only  global labels are added for the x and y axes. Empty lists specify default arguments. In this case, panel titles are given as capitalised letters or numbers (if there are more than 26 predictors), in bold, plus the grouping level in brackets; e.g., \strong{A} (Group One) or \strong{1} (Group One). X and y axis labels are simply given as \code{x} and \code{y}. A \code{fun} element in \code{add_main} is permitted which acts on group names (e.g., \code{\link[grDevices]{italic}} via \code{function(x) bquote(italic(.(x))))}). Alternatively, names can be specified via the `text' argument to \code{\link[graphics]{mtext}}. \code{NULL} suppress these arguments.
+#' @param add_main_box A logical variable that defines whether or not to add the title for each panel in a box at the top of the panel.
 #' @param add_additional A function used to add additional elements to each panel. This must accept two arguments, even if they are ignored: (1) a list of axis parameters (from \code{\link[prettyGraphics]{pretty_axis}}) and (2) the group-specific dataframe (see examples).
 #' @param par_param A list of arguments for \code{\link[graphics]{par}} to customise the plotting window. If unsupplied, default settings defined inside the function are used.
 #' @param by_row A logical variable that defines whether to order panels by row (\code{TRUE}) or column (\code{FALSE}).
@@ -76,6 +77,7 @@ pretty_panel <-
            add_xlab = NULL,
            add_ylab = NULL,
            add_main = NULL,
+           add_main_box = TRUE,
            add_additional = NULL,
            par_param = NULL, by_row = TRUE,...){
 
@@ -100,6 +102,7 @@ pretty_panel <-
     }
 
     #### Define plotting window
+    graphics::plot.new()
     par_param_init <- graphics::par(no.readonly = TRUE)
     on.exit(do.call(graphics::par, par_param_init), add = TRUE)
     if(is.null(par_param)) par_param = list()
@@ -120,10 +123,33 @@ pretty_panel <-
       if(by_row) par_param$mfrow <- mf else par_param$mfcol <- mf
     }
     pp <- do.call(graphics::par, par_param)
+    if(!is.null(add_main)){
+      add_main_fun <- add_main$fun
+      add_main$fun <- NULL
+      if(is.null(add_main_fun)) add_main_fun <- function(x) x
+      if(add_main_box){
+        if(is.null(add_main$bg)) add_main$bg <- "grey"
+        # if(is.null(add_main$adj)) add_main$adj <- c(0, 0)
+        # if(is.null(add_main$xjust)) add_main$xjust <- 0.5
+        # if(is.null(add_main$yjust)) add_main$yjust <- 0.5
+        # if(is.null(add_main$x.intersp)) add_main$x.intersp <- 0.5
+      }
+    }
 
     #### Define global param
     paa_box      <- pretty_axis_args
-    if(is.null(paa_box$x)) paa_box$x <- list(x = data$x, y = data$y)
+    if(is.null(paa_box$x)){
+      paa_box$x <- list(x = data$x, y = data$y)
+      if(!is.null(add_main) && add_main_box){
+        add_main_trial <- add_main
+        if(is.null(add_main_trial$legend)) add_main_trial$legend <- "TEST"
+        add_main_trial$x    <- "top"
+        add_main_trial$plot <- FALSE
+        lx <- do.call(graphics::legend, add_main_trial)
+        paa_box$x <- list(x = range(data$x),
+                          y = range(c(data$y, data$y + lx$rect$h)))
+      }
+    }
     paa_box$side <- 1:4
     paa_box$add  <- FALSE
 
@@ -139,17 +165,25 @@ pretty_panel <-
       axis_ls <- pretty_plot(d$x, d$y,
                              xlim = xlim, ylim = ylim,
                              pretty_axis_args = paa_box,...)
+      if(!is.null(add_main) && add_main_box){
+        main_box_bottom <-
+          axis_ls[[2]]$lim[2] - lx$rect$h * (axis_ls[[2]]$lim[2] - axis_ls[[2]]$lim[1])
+        axis_ls <- lapply(axis_ls, function(elm){
+          if(elm$axis$side %in% c(2, 4)){
+            if(any(elm$axis$at > main_box_bottom)){
+              pos <- which(elm$axis$at < main_box_bottom)
+              elm$axis <- lapply(elm$axis, function(axs){
+                if(length(axs) == length(elm$axis$at)) axs <- axs[pos]
+                return(axs)
+              })
+            }
+          }
+          return(elm)
+        })
+      }
 
       ## Add additional elements
       if(!is.null(add_additional)) add_additional(axis_ls, d)
-
-      ## Add blank axes
-      axis_ls_marks <-
-        lapply(axis_ls, function(elm) {
-          elm$axis$labels <- FALSE
-          return(elm)
-        })
-      pretty_axis(axis_ls = axis_ls_marks, add = TRUE)
 
       ## Add labelled axes for plots on the left/bottom of the multi-panel figure
       # Define parameters/helper function
@@ -161,6 +195,8 @@ pretty_panel <-
       update_side_param <- function(side_param){
         lim        <- side_param$lim
         axis_param <- side_param$axis
+        axis_param$col <- NA
+        if(is.null(axis_param$col.ticks)) axis_param$col.ticks <- "black"
         at <- axis_param$at
         if(length(at) > 1){
           axis_param <- lapply(axis_param, function(elm){
@@ -172,7 +208,45 @@ pretty_panel <-
           })
         }
       }
-      # Add axis and titles
+
+      # Add blank axes
+      axis_ls_marks <-
+        lapply(axis_ls, function(elm) {
+          elm$axis$labels <- FALSE
+          return(elm)
+        })
+      pretty_axis(axis_ls = axis_ls_marks, add = TRUE)
+
+      # Add titles
+      if(!is.null(add_main)){
+        if(is.null(add_main$text)) {
+          if(length(n_groups) <= 26) {
+            add_main$text <-
+              bquote(bold(.(LETTERS[i])) ~ "(" * .(add_main_fun(as.character(d$group[1]))) * ")")
+          } else {
+            add_main$text <-
+              bquote(bold(.(i)) ~ "(" * .(add_main_fun(as.character(d$group[1]))) * ")")
+          }
+        }
+        if(!add_main_box){
+          if(is.null(add_main$side)) add_main$side <- 3
+          do.call(graphics::mtext, add_main)
+        }
+        if(add_main_box){
+          if(is.null(add_main$legend)) add_main$legend <- add_main$text
+          add_main$text <- NULL
+          if(is.null(add_main$x)) add_main$x <- axis_ls[[1]]$lim
+          if(is.null(add_main$y)) {
+            # The y position is given as the ylim[2] minus lx$rect$h * (ylim[2] - ylim[1])
+            # ... This scales lx$rect$h, defined above on the default 0 - 1 window
+            # ... to the size of the relevant plot.
+            add_main$y <- c(main_box_bottom, axis_ls[[2]]$lim[2])
+          }
+          do.call(graphics::legend, add_main)
+        }
+      }
+
+      # Add labelled axes
       if(1 %in% pretty_axis_args$side){
         axis_param_x <- update_side_param(axis_ls[[1]])
         if(i %in% bottom) do.call(graphics::axis, axis_param_x)
@@ -188,21 +262,6 @@ pretty_panel <-
       if(4 %in% pretty_axis_args$side){
         axis_param_y <- update_side_param(axis_ls[[4]])
         if(i %in% right) do.call(graphics::axis, axis_param_y)
-      }
-      ## Add titles
-      print(d$group[1])
-      if(!is.null(add_main)){
-        if(is.null(add_main$side)) add_main$side <- 3
-        if(is.null(add_main$text)) {
-          if(length(n_groups) <= 26) {
-            add_main$text <-
-              bquote(bold(.(LETTERS[i])) ~ "(" * .(as.character(d$group[1])) * ")")
-          } else {
-            add_main$text <-
-              bquote(bold(.(i)) ~ "(" * .(as.character(d$group[1])) * ")")
-          }
-        }
-        do.call(graphics::mtext, add_main)
       }
     }) %>% invisible()
 
